8cb088a9d03c97fa03bf54bff20b0b39239cf235
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2020-07-17 16:57:19 +0545

    Add files via upload

---
+++ Instance and Class Variable.py
@@ -0,0 +1,23 @@
+#Author - Roshan Ghimire
+
+#Instance_And_Class_Variable
+class Employee:
+    
+    inc = 2
+    def __init__(self,fname,lname,sal):
+        self.fname = fname
+        self.lname = lname
+        self.sal = sal
+    
+    def increase(self):
+        self.sal = self.sal * Employee.inc
+        
+roshan = Employee("Roshan","Ghimire",2000)
+ravi = Employee("Ravi","Yadav",2200)
+
+print(roshan.sal)   
+roshan.increase()         #We can increase the salary by using the new function called increase.
+print(roshan.sal)
+print(roshan.__dict__)    #We can print the dictionary values of the object.
+print(Employee.__dict__)  #We can also print the dictioary of the class.
+
