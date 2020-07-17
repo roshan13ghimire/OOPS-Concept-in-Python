2ad9696d6a35398184b77a1537b2e3c84ac4f6a1
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2020-07-17 14:21:05 +0545

    Add files via upload

---
+++ Creating a Class.py
@@ -0,0 +1,40 @@
+
+# Author - Roshan Ghimire
+
+
+
+#Creating_a_Class
+class Employee:
+    pass
+
+roshan = Employee()
+ravi = Employee()
+
+roshan.fname = "Roshan"    
+ravi.fname = "Ravi"
+
+roshan.lname = "Ghimire"
+ravi.lname = "Yadav"
+
+roshan.age = "20"
+ravi.age = "22"
+
+print(roshan.age)
+print(roshan.fname)
+
+
+#Instead of writing these objects (fname,lname and age) two times,simply create a class and define these inside it as below.
+
+
+
+class Employee:
+    def __init__(self,fname,lname,age):
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+        
+roshan = Employee("Roshan","Ghimire",20)
+ravi = Employee("Ravi","Yadav",22)
+
+print(roshan.fname)
+
