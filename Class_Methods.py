27e818f13c3daf1cbce485c9cc62a69632c19eba
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2020-07-17 17:14:44 +0545

    Add files via upload

---
+++ Class_Methods.py
@@ -0,0 +1,30 @@
+#Class_Methods
+
+
+#When we want to change  some variables of the class, We use ClassMethod.
+
+
+class Employee:
+    
+    inc = 2 
+    def __init__(self,fname,lname,sal):
+        self.fname = fname
+        self.lname = lname
+        self.sal = sal
+    
+    
+    def increase(self):
+        self.sal = self.sal * Employee.inc
+     
+    @classmethod              #ClassMethod
+    def change_sal(ch,amount):
+        ch.inc = amount 
+        
+roshan = Employee("Roshan","Ghimire",2000)
+ravi = Employee("Ravi","Yadav",2200)
+
+print(roshan.sal)
+Employee.change_sal(2)
+roshan.increase()
+print(roshan.sal)
+
