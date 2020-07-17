7277648164aa9139de2f517073d69fae4f129e27
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2020-07-17 17:48:59 +0545

    Add files via upload

---
+++ Dunder_Method.py
@@ -0,0 +1,18 @@
+#Author - Roshan Ghimire
+
+#Dunder_Method
+
+
+class Employee:
+    def __init__(self,fname,lname,age):   #Constructor
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+    
+    def __add__(self,nextt):        #Dunder_Method
+        return self.age + nextt.age
+        
+roshan = Employee("Roshan","Ghimire",20)
+ravi = Employee("Ravi","Yadav",22)
+
+print(roshan + ravi)
